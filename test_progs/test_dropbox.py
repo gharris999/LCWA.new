82ae68d6782dd5a7c6df17c4be9c54a82eb1a4ca
andi klein <pabloemma@gmail.com>
2023-07-18 16:16:25 -0600

    changed key file name for test

--- test_progs/test_dropbox.py
+++ test_progs/test_dropbox.py
@@ -172,8 +172,8 @@ if __name__ == '__main__':
     import os.path
     loop_time       = 600 # every loop_time we will read a file and copy it locally, the time is in seconds
     homedir         = os.path.expanduser('~')
-    tokenfile       = homedir+'/git/LCWA/src/LCWA_d.txt' # the name and path of the dropbox creds
-    #tokenfile       = homedir+'/git/LCWA/src/LCWA_a.txt' # the name and path of the dropbox creds
+    #tokenfile       = homedir+'/git/LCWA/src/LCWA_d.txt' # the name and path of the dropbox creds
+    tokenfile       = homedir+'/git/LCWA/src/LCWA_a.txt' # the name and path of the dropbox creds
     #dropbox_dir     = '/LCWA/ALL_LCWA/' # dir on dropbox
     dropbox_dir     = '/LCWA/LC99_/'
     #dropbox_file    = 'LCWA_TOTAL_2023-05-07speedfile.pdf' # name of file
